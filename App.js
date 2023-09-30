5191875feb6f66d3a358281622f92d0f822d7db4
Blay <blay78792@gmail.com>
2023-09-30 13:53:18 +0000

    First version of HomeScreen

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
